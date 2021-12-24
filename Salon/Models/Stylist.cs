7f77f8dc8256f87e840b634edd956309fd943482
Aaron Minnick <abminnick@gmail.com>
2021-12-24 09:43:30 -0800

    add code to Models

--- Salon/Models/Stylist.cs
+++ Salon/Models/Stylist.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+
 namespace Salon.Models
 {
   public class Stylist
   {
-    
+    public int StylistId {get; set;}
+    public string FirstName {get; set;}
+    public string LastName {get; set;}
+
+    public virtual ICollection<Client> Clients {get; set;}
+
+    public Stylist()
+    {
+      Clients = new HashSet<Client>() {};
+    }
   }
 }
